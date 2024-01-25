61f82305f9d07e5afcdc75f8431f4d246de01f27
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-01-25 15:49:03 +0530

    initial commit

---
+++ rollingdice/src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+
+function App(): React.JSX.Element {
+  return (
+    <View>
+      <Text>Hello world</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+});
+
+export default App;

a214e1492f8e64e326a620e86bd1eae42bba3c0d
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-03-06 23:22:44 +0530

    navigation setup

---
+++ VideoCallApp/src/Navigation/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import CallScreen from '../CallScreen';
+import ContactScreen from '../ContactScreen';
+import CallingScreen from '../CallingScreen';
+// import IncomingCallScreen from '../screens/IncomingCallScreen';
+import LoginScreen from '../HomeScreen/Login';
+
+const Stack = createNativeStackNavigator();
+
+const Navigation = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Contacts" component={ContactScreen} />
+
+        <Stack.Group screenOptions={{headerShown: false}}>
+          <Stack.Screen name="Call" component={CallScreen} />
+          <Stack.Screen name="Calling" component={CallingScreen} />
+          {/* <Stack.Screen name="IncomingCall" component={IncomingCallScreen} /> */}
+        </Stack.Group> 
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Navigation;

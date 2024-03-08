760e5df7593dad59e4050a293f9d9d9e1253881d
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-03-08 23:50:29 +0530

    camera reverse toggle

--- VideoCallApp/src/components/CallActionBox/index.js
+++ VideoCallApp/src/components/CallActionBox/index.js
@@ -2,17 +2,26 @@ import React, {useState} from 'react';
 import {View, StyleSheet, Pressable} from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import MaterialIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import {Voximplant} from 'react-native-voximplant';
 
 const CallActionBox = ({onHangupPress}) => {
   const [isCameraOn, setIsCameraOn] = useState(true);
   const [isMicOn, setIsMicOn] = useState(true);
+  const [cameraType, setCameraType] = useState('BACK');
 
   const onReverseCamera = () => {
-    console.warn('onReverseCamera');
+    if(cameraType === 'FRONT'){
+      setCameraType('BACK');
+      Voximplant.Hardware.CameraManager.getInstance().switchCamera(Voximplant.Hardware.CameraType.BACK);
+    }else{
+      setCameraType('FRONT');
+      Voximplant.Hardware.CameraManager.getInstance().switchCamera(Voximplant.Hardware.CameraType.FRONT);
+    }
+    
   };
 
-  const onToggleCamera = () => {
-    setIsCameraOn(currentValue => !currentValue);
+  const onToggleCamera = async () => {
+    setIsCameraOn(curr => !curr);
   };
 
   const onToggleMicrophone = () => {

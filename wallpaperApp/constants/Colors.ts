bb0c98b0688fa2492c4e83d1850656ada81556d9
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-11-05 23:59:51 +0530

    color altered

--- wallpaperApp/constants/Colors.ts
+++ wallpaperApp/constants/Colors.ts
@@ -9,7 +9,7 @@ const tintColorDark = '#fff';
 export const Colors = {
   light: {
     text: '#11181C',
-    background: '#fff',
+    background: '#EFEFEF',
     tint: tintColorLight,
     icon: '#687076',
     tabIconDefault: '#687076',
@@ -17,7 +17,7 @@ export const Colors = {
   },
   dark: {
     text: '#ECEDEE',
-    background: '#151718',
+    background: '#333437',
     tint: tintColorDark,
     icon: '#9BA1A6',
     tabIconDefault: '#9BA1A6',

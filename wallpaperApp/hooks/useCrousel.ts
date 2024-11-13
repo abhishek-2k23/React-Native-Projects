d574f9a81028ced9b13319dc1ec636b4646c41f3
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-11-13 19:23:21 +0530

    craousel with title

--- wallpaperApp/hooks/useCrousel.ts
+++ wallpaperApp/hooks/useCrousel.ts
@@ -1,3 +1,34 @@
-export default function useCrousel(): string[]{
-    return ['https://ideogram.ai/assets/progressive-image/balanced/response/7kpEyFndTr-DfWg4Y9e60A','https://ideogram.ai/assets/progressive-image/balanced/response/1siurjNOSGiSxDdh0v8kRw','https://ideogram.ai/assets/progressive-image/balanced/response/mr4XlOQeSjOwXchMnIE2dg','https://ideogram.ai/assets/progressive-image/balanced/response/_gpD9AQoS8qu1-gJJOPUhA','https://ideogram.ai/assets/progressive-image/balanced/response/w12NeqTMRG62FH2HA7635g', 'https://ideogram.ai/assets/progressive-image/balanced/response/MaCtpA8-TWSl-9vwI0VKrQ']
+export default function useCrousel(): { title: string; image: string }[] {
+  return [
+    {
+      title: "title",
+      image:
+        "https://ideogram.ai/assets/progressive-image/balanced/response/7kpEyFndTr-DfWg4Y9e60A",
+    },
+    {
+      title: "title",
+      image:
+        "https://ideogram.ai/assets/progressive-image/balanced/response/1siurjNOSGiSxDdh0v8kRw",
+    },
+    {
+      title: "title",
+      image:
+        "https://ideogram.ai/assets/progressive-image/balanced/response/mr4XlOQeSjOwXchMnIE2dg",
+    },
+    {
+      title: "title",
+      image:
+        "https://ideogram.ai/assets/progressive-image/balanced/response/_gpD9AQoS8qu1-gJJOPUhA",
+    },
+    {
+      title: "title",
+      image:
+        "https://ideogram.ai/assets/progressive-image/balanced/response/w12NeqTMRG62FH2HA7635g",
+    },
+    {
+      title: "title",
+      image:
+        "https://ideogram.ai/assets/progressive-image/balanced/response/MaCtpA8-TWSl-9vwI0VKrQ",
+    },
+  ]
 }

def6b1a439421eae345aba12eebf36e737cc8d1c
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-11-13 19:23:51 +0530

    BottomSheet using react-native-modal

--- wallpaperApp/components/src/DownloadPhoto.tsx
+++ wallpaperApp/components/src/DownloadPhoto.tsx
@@ -1,27 +1,40 @@
-import {StyleSheet} from "react-native";
-import Modal from "react-native-modal";
-import BottomSheetContent from "./BottomSheetContent";
-import { Wallpaper } from "@/hooks/useWallpaper";
+import { StyleSheet } from "react-native"
+import Modal from "react-native-modal"
+import BottomSheetContent from "./BottomSheetContent"
+import { Wallpaper } from "@/hooks/useWallpaper"
 
-const DownloadPhoto = ({onClose, isVisible, wallpaper}: {onClose: () => void; isVisible: boolean; wallpaper: Wallpaper}) => {
+const DownloadPhoto = ({
+  onClose,
+  isVisible,
+  wallpaper,
+}: {
+  onClose: () => void
+  isVisible: boolean
+  wallpaper: Wallpaper
+}) => {
   return (
-    <Modal isVisible={isVisible} style={styles.modalContainer} onModalHide={onClose} onSwipeCancel={onClose} onBackButtonPress={onClose}>
-       
-       <BottomSheetContent wallpaper={wallpaper} onClose={onClose}/>
-      </Modal>
+    <Modal
+      isVisible={isVisible}
+      style={styles.modalContainer}
+      onModalHide={onClose}
+      onSwipeCancel={onClose}
+      onBackButtonPress={onClose}
+    >
+      <BottomSheetContent wallpaper={wallpaper} onClose={onClose} />
+    </Modal>
   )
 }
 
 export default DownloadPhoto
 
 const styles = StyleSheet.create({
-    modalContainer: {
-        ...StyleSheet.absoluteFillObject,
-        margin:0,
-        position: 'absolute',
-        flex: 1, 
-        height: '100%', 
-        bottom: 0, 
-        width: '100%'     
-    }
+  modalContainer: {
+    ...StyleSheet.absoluteFillObject,
+    margin: 0,
+    position: "absolute",
+    flex: 1,
+    height: "100%",
+    bottom: 0,
+    width: "100%",
+  },
 })

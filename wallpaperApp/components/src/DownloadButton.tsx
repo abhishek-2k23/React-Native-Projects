7ddf74b5aac9427c06836f7b7dcd70c2779e446f
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-11-15 00:24:57 +0530

    media download half done

--- wallpaperApp/components/src/DownloadButton.tsx
+++ wallpaperApp/components/src/DownloadButton.tsx
@@ -3,22 +3,41 @@ import { ThemedView } from "../ThemedView"
 import { Ionicons } from "@expo/vector-icons"
 import { ThemedText } from "../ThemedText"
 import { TouchableOpacity } from "react-native-gesture-handler"
-import * as expomedia from 'expo-media-library';
-export function DownloadButton({url} : {url: string}) {
-    const theme = useColorScheme() ?? "light"
-    return (
-      <Pressable onPress={async () => {
-        const response  = await expomedia.getPermissionsAsync(true);
-        if(response.granted){
-          const downloadResonse = await expomedia.saveToLibraryAsync(url);
-          console.log('download completed', downloadResonse)
-        }else{
-          console.log('response not granted')
-          console.log(response)
-        }
-      }}>
+import * as MediaLibrary from "expo-media-library"
+import * as FileSystem from 'expo-file-system';
 
-      
+export function DownloadButton({ url }: { url: string }) {
+  const theme = useColorScheme() ?? "light"
+  const [status, requestPermission] = MediaLibrary.usePermissions()
+  return (
+    <Pressable
+      onPress={async () => {
+        try {
+
+          // check for the permissions
+          if (!status?.granted) {
+            const permissionRes = await requestPermission()
+            if (!permissionRes.granted) {
+              console.log("Permission not granted")
+            }
+          } else {
+            const downloadResonse = await MediaLibrary.saveToLibraryAsync(url)
+            console.log(downloadResonse);
+          }
+          console.log(status);
+          //download the image
+          const fileUri=  FileSystem.documentDirectory+Date.now().toString()+"_download.jpg";
+          const {uri} = await FileSystem.downloadAsync(url,fileUri);
+          console.log(uri)
+          //save the downloaded image to gallery
+          const asset = await MediaLibrary.createAssetAsync(uri);
+          console.log("downloaded")
+        } catch (e){
+          console.log("response not granted")
+          console.log(e)
+        }
+      }}
+    >
       <ThemedView
         style={[
           styles.downloadButton,
@@ -36,18 +55,18 @@ export function DownloadButton({url} : {url: string}) {
           Download
         </ThemedText>
       </ThemedView>
-      </Pressable>
-    )
-  }
-  
-  const styles = StyleSheet.create({
-    downloadButton: {
-      width: "90%",
-      flexDirection: "row",
-      justifyContent: "center",
-      alignItems: "center",
-      gap: 5,
-      borderRadius: 10,
-      paddingVertical: 10,
-    },
-  })
+    </Pressable>
+  )
+}
+
+const styles = StyleSheet.create({
+  downloadButton: {
+    width: "90%",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 5,
+    borderRadius: 10,
+    paddingVertical: 10,
+  },
+})

5ca1d45ab3a1d643df9b731cd5da0626ada02348
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-11-23 23:56:57 +0530

    axios client

---
+++ wallpaperApp/services/GlobalApi.tsx
@@ -0,0 +1,16 @@
+import axios from "axios"
+
+const axiosClient = axios.create({
+  baseURL: "http://192.168.77.241:1337/api",
+  headers: {
+    Authorization: "Bearer " + process.env.EXPO_PUBLIC_STRAPI_API_TOKEN,
+  },
+})
+
+const getUserInfo =  (email: string) => {
+  console.log("inside get user info")
+  axiosClient.get("/user-lists?find[userEmail][$eq]=" + email)
+}
+const createUser = (data: { userEmail: string; userName: string }) =>
+  axiosClient.post("user-lists?create")
+export default { getUserInfo }

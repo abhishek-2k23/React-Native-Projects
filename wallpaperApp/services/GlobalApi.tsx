1a764cb5496dba45c49561da4f290731647e5f9d
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-11-24 22:23:20 +0530

    api function for creating user and also errors fixed

--- wallpaperApp/services/GlobalApi.tsx
+++ wallpaperApp/services/GlobalApi.tsx
@@ -3,14 +3,14 @@ import axios from "axios"
 const axiosClient = axios.create({
   baseURL: "http://192.168.77.241:1337/api",
   headers: {
-    Authorization: "Bearer " + process.env.EXPO_PUBLIC_STRAPI_API_TOKEN,
+    'Authorization': 'Bearer ' + process.env.EXPO_PUBLIC_STRAPI_API_TOKEN,
   },
 })
 
 const getUserInfo =  (email: string) => {
-  console.log("inside get user info")
-  axiosClient.get("/user-lists?find[userEmail][$eq]=" + email)
+  return axiosClient.get("/user-lists?find[userEmail][$eq]=" + email)
 }
 const createUser = (data: { userEmail: string; userName: string }) =>
-  axiosClient.post("user-lists?create")
-export default { getUserInfo }
+  axiosClient.post("user-lists",{data: data})
+
+export default { getUserInfo, createUser}
